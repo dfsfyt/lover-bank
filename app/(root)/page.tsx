0281fd5fd646bd2528540c150407e6eb1a6d5936
dfsfyt <dfsfyt@163.com>
2024-10-04 16:35:19 +0800

    implament the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,28 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
+import TotalBalance from '@/components/TotalBalance';
 import { NextPage } from 'next'
 import React from 'react'
 
 const Home: NextPage = () => {
+  const loggedIn = { firstName: 'DFSFYT', lastName: 'FAN', email: 'dfsfyt@163.com' };
   return (
-    <div>
-      Home
-    </div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox type='greeting' title='Welcome' user={loggedIn.firstName || 'Guest'} subtext='Access and manage your account and transactions efficiently.' />
+          <TotalBalance accounts={[]} totalBanks={1} totalCurrentBalance={1250.35} />
+        </header>
+
+        RECENT TRANSACTIONS
+      </div>
+
+      <RightSidebar 
+        user={loggedIn}
+        transactions={[]}
+        banks={[{currentBalance: 123.50}, {currentBalance: 500.00}]}
+      />
+    </section>
   )
 }
 

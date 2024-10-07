2ad83d684b2c2132eba83c71f69c606a7d615421
dfsfyt <dfsfyt@163.com>
2024-10-08 00:18:32 +0800

    img test

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -8,7 +8,7 @@ const RightSidebar = ({user, transactions, banks}: RightSidebarProps) => {
   return (
     <aside className='right-sidebar'>
       <section className='flex flex-col pb-8'>
-        <div className='profile-banner'></div>
+        {/* <div className='profile-banner'></div> */}
         <div className='profile'>
             <div className='profile-img'>
                 <span className='text-5xl font-bold text-blue-500'>{user.firstName[0]}</span>

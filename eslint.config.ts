a82c5dfcc51c152a316c2eda087596db7101a329
dfsfyt <dfsfyt@163.com>
2024-10-09 19:58:39 +0800

    eslint config

---
+++ eslint.config.ts
@@ -0,0 +1,9 @@
+export default [
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "error",
+      "@typescript-eslint/no-empty-object-type": "error",
+      "@typescript-eslint/no-explicit-any": "error"
+    },
+  },
+];

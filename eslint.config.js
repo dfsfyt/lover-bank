9d4a7b60ea3942292fe1aad8c3618365859f3308
dfsfyt <dfsfyt@163.com>
2024-10-09 22:31:10 +0800

    eslint-config 2

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ module.exports = [
     plugins: ["@typescript-eslint"],
     extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
     rules: {
-      "@typescript-eslint/no-unused-vars": "error",
+      "@typescript-eslint/no-unused-vars": ["error"],
       "no-unused-vars": "off",
       "@typescript-eslint/no-empty-object-type": "error",
       "@typescript-eslint/no-explicit-any": "off",

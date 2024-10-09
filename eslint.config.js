a0104f0363dfa1592e3d28c36f16efe9538d1b40
dfsfyt <dfsfyt@163.com>
2024-10-09 22:18:35 +0800

    no-unused-vars

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ module.exports = [
   {
     rules: {
       "@typescript-eslint/no-unused-vars": "error",
+      "no-unused-vars": "off",
       "@typescript-eslint/no-empty-object-type": "error",
       "@typescript-eslint/no-explicit-any": "off"
     },
